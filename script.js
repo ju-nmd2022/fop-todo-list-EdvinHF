54bf0bcf85d72e9f393617d254121621a5658598
EdvinHF <Hoed22rl@student.ju.se>
2023-04-10 16:05:36 +0200

    one chore

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+function addChore() {
+  const inputElement = document.getElementById("input");
+  const chore = inputElement.value;
+  const choreListElement = document.getElementById("choreList");
+  choreListElement.innerText = chore;
+}
 function loadHandler() {
   const button = document.getElementById("button");
+  button.addEventListener("click", addChore);
 }
 window.addEventListener("load", loadHandler);
