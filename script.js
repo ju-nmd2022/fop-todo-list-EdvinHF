1181e760836d0c0b2a9ec4b2feb45853d30092d8
EdvinHF <Hoed22rl@student.ju.se>
2023-04-10 18:51:18 +0200

    remove items

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+let uniqeId = 0;
+
 function addChore() {
+  uniqeId = uniqeId + 1;
+
   const inputElement = document.getElementById("input");
-  const chore = inputElement.value;
-  console.log(chore);
+  let chore = inputElement.value;
+  inputElement.value = "";
   const choreItem = document.createElement("li");
+  const removeButton = document.createElement("button");
+  choreItem.id = "li" + uniqeId;
+  removeButton.id = "button" + uniqeId;
   const choreText = document.createTextNode(chore);
   choreItem.appendChild(choreText);
   const choreListElement = document.getElementById("choreList");
   choreListElement.appendChild(choreItem);
+  choreListElement.appendChild(removeButton);
+  removeButton.addEventListener("click", removeButton1);
+  function removeButton1() {
+    choreItem.remove();
+    removeButton.remove();
+    console.log(removeButton);
+  }
 }
 function loadHandler() {
   const button = document.getElementById("button");
