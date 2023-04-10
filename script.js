b696dc1dd1892ccb50e494c50622489d875159f9
EdvinHF <Hoed22rl@student.ju.se>
2023-04-10 23:16:48 +0200

    fdsd

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let uniqeId = 0;
-
+function removeButton1() {
+  choreItem.remove();
+  removeButton.remove();
+}
 function addChore() {
   uniqeId = uniqeId + 1;
 
@@ -8,6 +11,8 @@ function addChore() {
   inputElement.value = "";
   const choreItem = document.createElement("li");
   const removeButton = document.createElement("button");
+  const doneButton = document.createElement("button");
+  doneButton.id = "done" + uniqeId;
   choreItem.id = "li" + uniqeId;
   removeButton.id = "button" + uniqeId;
   const choreText = document.createTextNode(chore);
@@ -15,11 +20,20 @@ function addChore() {
   const choreListElement = document.getElementById("choreList");
   choreListElement.appendChild(choreItem);
   choreListElement.appendChild(removeButton);
+  choreListElement.appendChild(doneButton);
   removeButton.addEventListener("click", removeButton1);
   function removeButton1() {
     choreItem.remove();
     removeButton.remove();
-    console.log(removeButton);
+    doneButton.remove();
+  }
+  doneButton.addEventListener("click", doneButton1);
+  function doneButton1() {
+    doneButton.remove();
+    choreItem.style.textDecoration = "line-through";
+  }
+  if (inputElement.value > 1) {
+    console.log("hi");
   }
 }
 function loadHandler() {
