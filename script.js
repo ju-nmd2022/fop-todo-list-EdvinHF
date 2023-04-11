120ba6a559db3747eca216eb777003e22c21aca0
EdvinHF <Hoed22rl@student.ju.se>
2023-04-11 16:10:30 +0200

    input needs value

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
 let uniqeId = 0;
-function removeButton1() {
-  choreItem.remove();
-  removeButton.remove();
-}
+
 function addChore() {
   uniqeId = uniqeId + 1;
 
   const inputElement = document.getElementById("input");
   let chore = inputElement.value;
   inputElement.value = "";
-  const choreItem = document.createElement("li");
-  const removeButton = document.createElement("button");
-  const doneButton = document.createElement("button");
-  doneButton.id = "done" + uniqeId;
-  choreItem.id = "li" + uniqeId;
-  removeButton.id = "button" + uniqeId;
-  const choreText = document.createTextNode(chore);
-  choreItem.appendChild(choreText);
-  const choreListElement = document.getElementById("choreList");
-  choreListElement.appendChild(choreItem);
-  choreListElement.appendChild(removeButton);
-  choreListElement.appendChild(doneButton);
-  removeButton.addEventListener("click", removeButton1);
-  function removeButton1() {
-    choreItem.remove();
-    removeButton.remove();
-    doneButton.remove();
-  }
-  doneButton.addEventListener("click", doneButton1);
-  function doneButton1() {
-    doneButton.remove();
-    choreItem.style.textDecoration = "line-through";
-  }
-  if (inputElement.value > 1) {
-    console.log("hi");
+  if (chore !== "") {
+    const choreItem = document.createElement("li");
+    const removeButton = document.createElement("button");
+    const doneButton = document.createElement("button");
+    const choreText = document.createTextNode(chore);
+    doneButton.id = "done" + uniqeId;
+    choreItem.id = "li" + uniqeId;
+    removeButton.id = "button" + uniqeId;
+    choreItem.appendChild(choreText);
+    const choreListElement = document.getElementById("choreList");
+    choreListElement.appendChild(choreItem);
+    choreListElement.appendChild(removeButton);
+    choreListElement.appendChild(doneButton);
+    removeButton.addEventListener("click", removeButton1);
+    function removeButton1() {
+      choreItem.remove();
+      removeButton.remove();
+      doneButton.remove();
+    }
+    doneButton.addEventListener("click", doneButton1);
+    function doneButton1() {
+      doneButton.remove();
+      choreItem.style.textDecoration = "line-through";
+    }
   }
 }
 function loadHandler() {
