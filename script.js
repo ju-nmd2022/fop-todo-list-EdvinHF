cbe4d857a8476a0adf04ae7b994c991d668573d7
EdvinHF <Hoed22rl@student.ju.se>
2023-04-12 02:03:55 +0200

    damn

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-let uniqeId = 0;
-
 function addChore() {
-  uniqeId = uniqeId + 1;
   const inputElement = document.getElementById("input");
-  let chore = inputElement.value;
+  const chore = inputElement.value;
   inputElement.value = "";
+  localStorage.chore = inputElement.value;
   if (chore !== "") {
+    const choreListElement = document.getElementById("choreList");
     const choreItem = document.createElement("li");
     const removeButton = document.createElement("button");
     const doneButton = document.createElement("button");
-    const choreText = document.createTextNode(chore);
-    choreItem.id = "li" + uniqeId;
-    doneButton.id = "done" + uniqeId;
-    doneButton.id = "done";
+    const choreContainer = document.createElement("div");
+    choreContainer.classList.add("choreContainer");
     removeButton.classList.add("remove");
     doneButton.classList.add("done");
-    choreItem.appendChild(choreText);
-    const choreListElement = document.getElementById("choreList");
-    choreListElement.appendChild(choreItem);
-    choreListElement.appendChild(removeButton);
-    choreListElement.appendChild(doneButton);
+    choreItem.innerHTML = chore;
+    choreListElement.appendChild(choreContainer);
+    choreContainer.appendChild(choreItem);
+    choreContainer.appendChild(removeButton);
+    choreContainer.appendChild(doneButton);
     removeButton.addEventListener("click", removeButton1);
     function removeButton1() {
       choreItem.remove();
       removeButton.remove();
       doneButton.remove();
+      choreContainer.remove();
     }
     doneButton.addEventListener("click", doneButton1);
     function doneButton1() {
@@ -33,8 +31,10 @@ function addChore() {
     }
   }
 }
+function storeList() {}
 function loadHandler() {
   const button = document.getElementById("button");
   button.addEventListener("click", addChore);
+  storeList();
 }
 window.addEventListener("load", loadHandler);
