922913fe58321a899fca902d05fb4245cf1ddd31
EdvinHF <Hoed22rl@student.ju.se>
2023-04-24 00:28:46 +0200

    json local storage

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ function addChore() {
   const inputElement = document.getElementById("input");
   const chore = inputElement.value;
   inputElement.value = "";
-  localStorage.chore = inputElement.value;
+
   if (chore !== "") {
     const choreListElement = document.getElementById("choreList");
     const choreItem = document.createElement("li");
     const removeButton = document.createElement("button");
     const doneButton = document.createElement("button");
     const choreContainer = document.createElement("div");
+
     choreContainer.classList.add("choreContainer");
     removeButton.classList.add("remove");
     doneButton.classList.add("done");
@@ -17,24 +18,75 @@ function addChore() {
     choreContainer.appendChild(choreItem);
     choreContainer.appendChild(removeButton);
     choreContainer.appendChild(doneButton);
-    removeButton.addEventListener("click", removeButton1);
-    function removeButton1() {
+
+    removeButton.addEventListener("click", removeButtonEvent);
+    function removeButtonEvent() {
       choreItem.remove();
       removeButton.remove();
       doneButton.remove();
       choreContainer.remove();
+      updateList();
     }
-    doneButton.addEventListener("click", doneButton1);
-    function doneButton1() {
+
+    doneButton.addEventListener("click", doneButtonEvent);
+    function doneButtonEvent() {
+      doneButton.remove();
+      choreItem.style.textDecoration = "line-through";
+    }
+
+    updateList();
+  }
+}
+
+function updateList() {
+  const chores = [];
+  const choreItems = document.querySelectorAll(".choreContainer li");
+  for (let i = 0; i < choreItems.length; i++) {
+    const chore = choreItems[i].innerHTML;
+    chores.push(chore);
+  }
+  localStorage.setItem("chores", JSON.stringify(chores));
+}
+
+function loadList() {
+  const choreListElement = document.getElementById("choreList");
+  const chores = JSON.parse(localStorage.getItem("chores")) || [];
+
+  for (let i = 0; i < chores.length; i++) {
+    const chore = chores[i];
+    const choreItem = document.createElement("li");
+    const removeButton = document.createElement("button");
+    const doneButton = document.createElement("button");
+    const choreContainer = document.createElement("div");
+
+    choreContainer.classList.add("choreContainer");
+    removeButton.classList.add("remove");
+    doneButton.classList.add("done");
+    choreItem.innerHTML = chore;
+    choreListElement.appendChild(choreContainer);
+    choreContainer.appendChild(choreItem);
+    choreContainer.appendChild(removeButton);
+    choreContainer.appendChild(doneButton);
+
+    removeButton.addEventListener("click", removeButtonEvent);
+    function removeButtonEvent() {
+      choreItem.remove();
+      removeButton.remove();
+      doneButton.remove();
+      choreContainer.remove();
+      updateList();
+    }
+
+    doneButton.addEventListener("click", doneButtonEvent);
+    function doneButtonEvent() {
       doneButton.remove();
       choreItem.style.textDecoration = "line-through";
     }
   }
 }
-function storeList() {}
 function loadHandler() {
   const button = document.getElementById("button");
   button.addEventListener("click", addChore);
-  storeList();
+  loadList();
 }
 window.addEventListener("load", loadHandler);
